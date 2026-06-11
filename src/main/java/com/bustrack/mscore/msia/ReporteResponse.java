7a9f3cc6b1f2f8d9517f0b40183cab753b2b2880
Black7in <elperroblack158@gmail.com>
2026-06-11 03:12:22 -0400

    Ignore unknown fields in MS-IA ReporteResponse (sql_generado etc)

--- src/main/java/com/bustrack/mscore/msia/ReporteResponse.java
+++ src/main/java/com/bustrack/mscore/msia/ReporteResponse.java
@@ -1,7 +1,9 @@
 package com.bustrack.mscore.msia;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ReporteResponse(
         String pregunta,
         String explicacion,
